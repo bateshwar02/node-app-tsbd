013aeaad057f3dc45ca38fe106844412312bc6fc
Bateshwar <bateshwar.mishra@sapple.co.in>
2019-05-05 23:35:14 +0530

    deploy to heroku

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const publicDirectoryPath = path.join(__dirname,'../public');
-
 const app = express();
+const port = process.env.PORT || 3000
 // app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
@@ -12,8 +13,26 @@ app.use(express.static(publicDirectoryPath));
 app.get('',(req, res) => {
   res.render('index');
 })
+app.get('/home',(req, res) => {
+  res.render('index');
+})
+app.get('/admission',(req, res) => {
+  res.render('admission');
+})
+app.get('/staff',(req, res) => {
+  res.render('staff');
+})
+app.get('/contact',(req, res) => {
+  res.render('contact');
+})
+app.get('*', (req, res)=>{
+  res.render('404',{
+    title:'404',
+    name:'Page not Found',
+    errorMessage:'Page not Found'
+  })
+})
 
-
-app.listen(3000,()=>{
-    console.log('application server run on 3000 port');
+app.listen(port,()=>{
+    console.log('application server run on port'+port);
 })
